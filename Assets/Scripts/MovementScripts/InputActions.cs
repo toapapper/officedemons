4d0257eaebf6d9dd254e3ef40400ee16177b9d3f
johanmelkersson <melkersson.j@gmail.com>
2021-09-14 00:36:44 +0200

    playerInputAndplayerController
    player input and player controller (light), for when out of combat
    unsure if gamepad works, cant get my unity to detect my controller
    movement works ok

---
+++ Assets/Scripts/MovementScripts/InputActions.cs
@@ -0,0 +1,351 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/MovementScripts/InputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputActions"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerMovement"",
+            ""id"": ""38ef6f9e-86c3-4a3f-a044-b9fab1415b05"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""db05cccb-e03b-40c0-913f-b24ca7f59830"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""PickUp"",
+                    ""type"": ""Button"",
+                    ""id"": ""32ae0d17-dd45-471b-9a56-efb45bb1d2bd"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Attack"",
+                    ""type"": ""Button"",
+                    ""id"": ""34c17faf-0fed-49da-b113-ccc41047b271"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Special"",
+                    ""type"": ""Button"",
+                    ""id"": ""817ca354-7d6c-409b-b528-eca3e24fcd2e"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector [Gamepad]"",
+                    ""id"": ""a606952f-5dab-4269-b3cc-c930e36e3d5f"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""062f6bc6-8783-41fa-b351-542d0ec2955b"",
+                    ""path"": ""<Gamepad>/leftStick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""0f19f2b9-4531-4348-9d94-989954ba650c"",
+                    ""path"": ""<Gamepad>/leftStick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""0ab8f14c-fce2-4d74-b576-ba96cce48791"",
+                    ""path"": ""<Gamepad>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""381bd809-46a6-47cb-bdde-39b892bb1763"",
+                    ""path"": ""<Gamepad>/leftStick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""2D Vector [Keyboard]"",
+                    ""id"": ""6393aa6a-8a88-4f1e-82f6-41190f44c94e"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""ba92b1b4-ccd2-4adb-a071-c2298e59795d"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""3da43793-c615-451f-80d2-9b1f2b05ca7b"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""40903cd7-da32-44e7-aa9d-3ad24773df00"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""31514858-b1e1-44f1-8ae8-b8d88d255c93"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a70934ce-1ff4-4cd5-9234-15d5c3795a0b"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1cfb525c-7b81-4e86-8f26-6d4fd690dd6c"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""62113fe3-9107-433e-8fae-daac5015b7b8"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Special"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""184f3965-eba5-4a8e-81dd-cd34958c5e91"",
+                    ""path"": ""<Mouse>/rightButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Special"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9dbc30ad-e941-409e-8ee0-fe26ede70b84"",
+                    ""path"": ""<Gamepad>/buttonWest"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""PickUp"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""ef37025e-334a-464e-941d-e416a420bc66"",
+                    ""path"": ""<Keyboard>/e"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""PickUp"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // PlayerMovement
+        m_PlayerMovement = asset.FindActionMap("PlayerMovement", throwIfNotFound: true);
+        m_PlayerMovement_Move = m_PlayerMovement.FindAction("Move", throwIfNotFound: true);
+        m_PlayerMovement_PickUp = m_PlayerMovement.FindAction("PickUp", throwIfNotFound: true);
+        m_PlayerMovement_Attack = m_PlayerMovement.FindAction("Attack", throwIfNotFound: true);
+        m_PlayerMovement_Special = m_PlayerMovement.FindAction("Special", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerMovement
+    private readonly InputActionMap m_PlayerMovement;
+    private IPlayerMovementActions m_PlayerMovementActionsCallbackInterface;
+    private readonly InputAction m_PlayerMovement_Move;
+    private readonly InputAction m_PlayerMovement_PickUp;
+    private readonly InputAction m_PlayerMovement_Attack;
+    private readonly InputAction m_PlayerMovement_Special;
+    public struct PlayerMovementActions
+    {
+        private @InputActions m_Wrapper;
+        public PlayerMovementActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_PlayerMovement_Move;
+        public InputAction @PickUp => m_Wrapper.m_PlayerMovement_PickUp;
+        public InputAction @Attack => m_Wrapper.m_PlayerMovement_Attack;
+        public InputAction @Special => m_Wrapper.m_PlayerMovement_Special;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerMovement; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerMovementActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerMovementActions instance)
+        {
+            if (m_Wrapper.m_PlayerMovementActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnMove;
+                @PickUp.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnPickUp;
+                @PickUp.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnPickUp;
+                @PickUp.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnPickUp;
+                @Attack.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnAttack;
+                @Attack.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnAttack;
+                @Attack.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnAttack;
+                @Special.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnSpecial;
+                @Special.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnSpecial;
+                @Special.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnSpecial;
+            }
+            m_Wrapper.m_PlayerMovementActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @PickUp.started += instance.OnPickUp;
+                @PickUp.performed += instance.OnPickUp;
+                @PickUp.canceled += instance.OnPickUp;
+                @Attack.started += instance.OnAttack;
+                @Attack.performed += instance.OnAttack;
+                @Attack.canceled += instance.OnAttack;
+                @Special.started += instance.OnSpecial;
+                @Special.performed += instance.OnSpecial;
+                @Special.canceled += instance.OnSpecial;
+            }
+        }
+    }
+    public PlayerMovementActions @PlayerMovement => new PlayerMovementActions(this);
+    public interface IPlayerMovementActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnPickUp(InputAction.CallbackContext context);
+        void OnAttack(InputAction.CallbackContext context);
+        void OnSpecial(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/MovementScripts/PlayerController.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+	//World transform
+	private Vector3 forward;
+	private Vector3 right;
+
+	//Inputs
+	private Vector2 moveInput;
+	private float pickupInput; 
+	private float attackInput;
+	private float specialInput;
+
+	//Playermovement
+	CharacterController character;
+	private Vector3 moveDirection;
+	private Vector3 moveAmount = Vector3.zero;
+	private Vector3 smoothMoveVelocity;
+	private Quaternion rotationDirection;
+	[SerializeField]
+	private float speed = 1f; //Movement speed
+	[SerializeField]
+	const float rotationSpeed = 500f; // Rotation speed
+
+	//Helper variables
+	private bool objectNearby;
+
+	private void Start()
+	{
+		character = GetComponent<CharacterController>();
+		forward = Camera.main.transform.forward;
+		forward.y = 0;
+		forward.Normalize();
+		right = new Vector3(forward.z, 0, -forward.x);
+	}
+
+	public void OnMove(InputAction.CallbackContext context)
+	{
+		moveInput = context.ReadValue<Vector2>();
+		moveDirection = (moveInput.x * right + moveInput.y * forward).normalized;
+	}
+	public void OnPickUp(InputAction.CallbackContext context)
+	{
+		if (objectNearby)
+		{
+			pickupInput = context.ReadValue<float>();
+		}
+		Debug.Log("Pick up" + pickupInput);
+	}
+	public void OnAttack(InputAction.CallbackContext context)
+	{
+		attackInput = context.ReadValue<float>();
+		Debug.Log("Attack" + attackInput);
+	}
+	public void OnSpeciel(InputAction.CallbackContext context)
+	{
+		specialInput = context.ReadValue<float>();
+		Debug.Log("Special" + specialInput);
+	}
+
+
+	private void FixedUpdate()
+	{
+		if (moveDirection != Vector3.zero)
+		{
+			CalculateRotation();
+		}
+		if(transform.rotation != rotationDirection)
+		{
+			PerformRotation();
+		}
+		CalculateMovement();
+		if(moveAmount != Vector3.zero)
+		{
+			PerformMovement();
+		}
+		
+	}
+
+
+	private void CalculateRotation()
+	{
+		rotationDirection = Quaternion.LookRotation(moveDirection, Vector3.up);
+	}
+	private void PerformRotation()
+	{
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationDirection, rotationSpeed * Time.fixedDeltaTime);
+	}
+	private void CalculateMovement()
+	{
+		Vector3 targetMoveAmount = moveDirection * speed;
+		moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+	}
+	private void PerformMovement()
+	{
+		Vector3 localMove = moveAmount * Time.fixedDeltaTime;
+		character.Move(localMove);
+	}
+}
