ac75342766fe05f02208b6e9bacece30e36e7e65
toapapper <62546257+toapapper@users.noreply.github.com>
2021-09-15 16:36:31 +0200

    nästan klart med en ful prototyp
    behöver mest bara att sakerna som knapparna ska göra finns kodade nu

---
+++ Assets/Scripts/Misc/SimpleSpin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSpin : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(100 * Time.deltaTime, 100 * Time.deltaTime , 100 * Time.deltaTime);
+    }
+}
