30dd855990e15055b9b2e90d07ff3106ea87299d
Zippsson <62258314+Zippsson@users.noreply.github.com>
2021-09-16 12:46:55 +0200

    Moved code out of BSPTree
    Created a new script: GenerateTerrain that holds the code for actual building of terrain from the data in the BSPTree.

--- Assets/Scripts/Procedural/BSPTree.cs
+++ Assets/Scripts/Procedural/BSPTree.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BSPTree : MonoBehaviour
 {
     Node root;
-    public GameObject cubePrefab;
-    public GameObject groundPrefab;
+    public GenerateTerrain generateTerrain;
+    //public GameObject cubePrefab;
+    //public GameObject groundPrefab;
     public List<Node> nodes;
-    public List<GameObject> cubes = new List<GameObject>();
+    //public List<GameObject> cubes = new List<GameObject>();
     public int generations = 3;
     int width, oldWidth;
     int height, oldHeight;
@@ -18,16 +18,13 @@ public class BSPTree : MonoBehaviour
     int missfallMultiplier;
     public int missfallTop = 6;
     Vector2 lastSize;
+    int lastDirection;
 
     private void Start()
     {
         width = Random.Range((int)widthLimits.x, (int)widthLimits.y);
         height = Random.Range((int)heightLimits.x, (int)heightLimits.y);
-    }
-
-    public void MakeBigBSP()
-    {
-        MakeBSP();
+        generateTerrain = GetComponent<GenerateTerrain>();
     }
 
     public void MakeBSP()
@@ -37,12 +34,12 @@ public class BSPTree : MonoBehaviour
         missfallMultiplier = 0;
         nodes = new List<Node>();
         root = new Node(new Vector2(width, height),lastSize);
-        GenerateGround(root);
+        generateTerrain.GenerateGround(root);
         BSP(root);
         for (int i = 0; i < nodes.Count; i++)
         {
             if (nodes[i].leaf)
-                GenerateObstacles(nodes[i]);
+                generateTerrain.GenerateObstacles(nodes[i], root, width, height);
         }
         Debug.Log(nodes.Count);
 
@@ -50,8 +47,6 @@ public class BSPTree : MonoBehaviour
         height = Random.Range((int)heightLimits.x, (int)heightLimits.y);
         GO();
     }
-
-
 
     public void BSP(Node node)
     {
@@ -79,7 +74,7 @@ public class BSPTree : MonoBehaviour
 
         Node node1, node2;
 
-        int split = 0;
+        int split;
         int missfall = Random.Range(missfallMultiplier, missfallTop);
         if (missfall == missfallMultiplier && node.generation > 2)
             return;
@@ -127,46 +122,7 @@ public class BSPTree : MonoBehaviour
             BSP(node2);
         }
     }
-    /// <summary>
-    /// Instansiates a ground model from a quad prefab.
-    /// </summary>
-    /// <param name="node"></param>
-    private void GenerateGround(Node node)
-    {
-        GameObject cube = Instantiate(groundPrefab, new Vector3(node.position.x, 1, node.position.y), Quaternion.identity);
-        cube.transform.localScale = new Vector3(node.size.x, node.size.y, 1);
-        cube.transform.rotation = Quaternion.Euler(90, 0, 0);
-        cube.gameObject.name = "Ground";
-        node.cube = cube;
-        cubes.Add(cube);
-    }
 
-    private void GenerateObstacles(Node node)
-    {
-        float x, y;
-        BufferMaker(out x, out y, node);
-        //If they become too small use this
-        //if (node.size.x * node.size.y < width * height / 200)
-        //    return;
-
-        if (TooCloseCheck(node,20))
-            return;
-
-        GameObject cube = Instantiate(cubePrefab, new Vector3(node.position.x, 1, node.position.y), Quaternion.identity);
-        cube.transform.localScale = new Vector3(x, 100, y);
-        node.cube = cube;
-        cube.transform.parent = root.cube.transform;
-        cubes.Add(cube);
-    }
-
-    private void BufferMaker(out float x, out float y, Node node)
-    {
-        float bufferX = node.size.x / 4;
-        float bufferY = node.size.y / 4;
-        x = Random.Range(bufferX, node.size.x - bufferX);
-        y = Random.Range(bufferY, node.size.y - bufferY);
-
-    }
     /// <summary>
     /// true for vertical
     /// false for horizontal
@@ -191,19 +147,6 @@ public class BSPTree : MonoBehaviour
         }
     }
 
-    private bool TooCloseCheck(Node node, float distanceMultiplier)
-    {
-        if (Mathf.Abs(node.origin.x - root.origin.x) < width / distanceMultiplier)
-            return true;
-
-        if (Mathf.Abs(node.origin.y - root.origin.y) < height / distanceMultiplier)
-            return true;
-
-        if (Mathf.Abs((node.origin.y - node.size.y) - (root.origin.y - root.size.y)) < height / distanceMultiplier)
-            return true;
-
-        return false;
-    }
 
     /// <summary>
     /// Get a random number between 0 and 2
@@ -220,8 +163,11 @@ public class BSPTree : MonoBehaviour
             d = Random.Range(0, 2);
             if (d == 0)
                 break;
-            else if (d == 1)
+            else if (d == 1 && lastDirection != 2)
                 break;
+            else if (d == 2 && lastDirection != 1)
+                break;
+  
         }
         return d;
     }
@@ -234,16 +180,22 @@ public class BSPTree : MonoBehaviour
             GoRight();
         else if (direction == 1)
             GoUp();
+        else if(direction == 2)
+            GoDown();
     }
     private void GoRight()
     {
         lastSize.x += oldWidth;
-        height = oldHeight;
         Debug.Log("Went right");
     }
     private void GoUp()
     {
         lastSize.y += height;
         Debug.Log("Went up");
+    }
+    private void GoDown()
+    {
+        lastSize.y -= oldHeight;
+        Debug.Log("Went down");
     }
 }
