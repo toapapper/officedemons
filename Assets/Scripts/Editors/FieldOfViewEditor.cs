39c2b7160461627e16ae8298af9715f01effcbbc
tila18 <43038427+tila18@users.noreply.github.com>
2021-09-23 12:12:41 +0200

    attributes and actions scripts

--- Assets/Scripts/Editors/FieldOfViewEditor.cs
+++ Assets/Scripts/Editors/FieldOfViewEditor.cs
@@ -5,18 +5,18 @@ public class FieldOfViewEditor : Editor
 {
     private void OnSceneGUI()
     {
-        FieldOfView fow = (FieldOfView)target;
+        FieldOfView fov = (FieldOfView)target;
         Handles.color = Color.white;
-        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
-        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
-        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
+        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
 
-        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
-        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
 
-        foreach (Transform visibleTarget in fow.visibleTargets)
+        foreach (GameObject visibleTarget in fov.visibleTargets)
         {
-            Handles.DrawLine(fow.transform.position, visibleTarget.position);
+            Handles.DrawLine(fov.transform.position, visibleTarget.transform.position);
         }
     }
 }
--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    FieldOfView fov;
+    Actions actions;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fov = GetComponent<FieldOfView>();
+        actions = GetComponent<Actions>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        List<GameObject> targetList = fov.visibleTargets;
+
+        if (targetList.Count > 0)
+        {
+            actions.Attack(); 
+        }
     }
 }
