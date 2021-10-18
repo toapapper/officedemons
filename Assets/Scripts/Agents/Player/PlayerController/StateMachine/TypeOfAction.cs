9f1b993d46b690e68c340ea1c7567c4ad22a9357
johanmelkersson <melkersson.j@gmail.com>
2021-10-18 13:29:32 +0200

    moreComments

--- Assets/Scripts/Agents/Player/PlayerController/StateMachine/TypeOfAction.cs
+++ Assets/Scripts/Agents/Player/PlayerController/StateMachine/TypeOfAction.cs
@@ -2,6 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// <para>
+/// Enum of all actions player can choose between
+/// </para> 
+///  <para>
+///  Author: Johan Melkersson
+/// </para>
+/// </summary>
+
+// Last Edited: 2021-10-12
 public enum TypeOfAction
 {
 	NOACTION,
