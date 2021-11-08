ef29f3a91b5d0c007f88de27facd29f2a2ce18d8
Zippsson <62258314+Zippsson@users.noreply.github.com>
2021-11-08 14:06:21 +0100

    BG_music events

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,14 +14,47 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_AMBIENCE_HALL_LARGE__ENTRANCE__OFFICE_BUILDING__MORNING__DOWNTOWN_CHICAGO__USA__LOOP_LRLSRS = 1546138968U;
-        static const AkUniqueID PLAY_DING = 2028513310U;
-        static const AkUniqueID PLAY_OFFICE_BOSSA_ELVIN_MATZ_15_10 = 450519720U;
+        static const AkUniqueID PLAY_COMBAT = 513571230U;
+        static const AkUniqueID PLAY_ROAMING = 3711372643U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace COMBAT
+        {
+            static const AkUniqueID GROUP = 2764240573U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID PART1 = 3177314147U;
+                static const AkUniqueID PART2 = 3177314144U;
+                static const AkUniqueID PART3 = 3177314145U;
+                static const AkUniqueID PART4 = 3177314150U;
+                static const AkUniqueID PART5 = 3177314151U;
+                static const AkUniqueID PART6 = 3177314148U;
+            } // namespace STATE
+        } // namespace COMBAT
+
+        namespace ROAMING
+        {
+            static const AkUniqueID GROUP = 3557636762U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID ROAMSTATE1 = 2874554374U;
+                static const AkUniqueID ROAMSTATE2 = 2874554373U;
+            } // namespace STATE
+        } // namespace ROAMING
+
+    } // namespace STATES
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID MUSIC = 3991942870U;
     } // namespace BANKS
 
     namespace BUSSES
