843f892834ba1c9d8c3fcb21aea24cda57f1cc2d
Jonas96Lundin <62259168+Jonas96Lundin@users.noreply.github.com>
2021-11-17 15:08:55 +0100

    Added tims terrible breath particle effect

--- Assets/Scripts/Weapons/SpecialWeapons/Breath/TerribleBreath.cs
+++ Assets/Scripts/Weapons/SpecialWeapons/Breath/TerribleBreath.cs
@@ -15,57 +15,73 @@ using UnityEngine;
 // Last Edited: 15-11-16
 public class TerribleBreath : AbstractSpecial
 {
-	[SerializeField]
-	private float viewDistance = 10f;
-	[SerializeField]
-	private float viewAngle = 100f;
-	[SerializeField]
-	private float damageMultiplier = 3f;
-	[SerializeField]
-	private float hitForceMultiplier = 10f;
-	[SerializeField]
-	GameObject mouth;
+    [SerializeField]
+    private float viewDistance = 10f;
+    [SerializeField]
+    private float viewAngle = 100f;
+    [SerializeField]
+    private float damageMultiplier = 3f;
+    [SerializeField]
+    private float hitForceMultiplier = 10f;
+    [SerializeField]
+    GameObject mouth;
+    [SerializeField]
+    private GameObject particleEffect;
 
-	public override void SetFOVSize()
-	{
-		specialController.FOV.ViewAngle = viewAngle;
-		specialController.FOV.ViewRadius = viewDistance;
-	}
+    public override void SetFOVSize()
+    {
+        specialController.FOV.ViewAngle = viewAngle;
+        specialController.FOV.ViewRadius = viewDistance;
+    }
 
-	public override void ToggleAim(bool isActive)
-	{
-		specialController.FOVVisualization.SetActive(isActive);
-	}
+    public override void ToggleAim(bool isActive)
+    {
+        specialController.FOVVisualization.SetActive(isActive);
+    }
 
-	public override void StartAttack()
-	{
-		specialController.Animator.SetTrigger("isStartSpecialBreath");
-	}
-	public override void Attack()
-	{
-		specialController.Animator.SetTrigger("isSpecialBreath");
-	}
+    public override void StartAttack()
+    {
+        specialController.Animator.SetTrigger("isStartSpecialBreath");
+    }
+    public override void Attack()
+    {
+        if (!particleEffect.activeSelf)
+        {
+            particleEffect.SetActive(true);
+            StartCoroutine(CountdownTime(1.5f));
 
-	public override void StartTurnEffect()
-	{
-		base.AddCharge();
-	}
-	public override void RevivedEffect()
-	{
-		Charges = MaxCharges;
-	}
+        }
+        specialController.Animator.SetTrigger("isSpecialBreath");
+    }
 
-	public override void DoSpecialAction()
-	{
-		if (specialController.FOV.VisibleTargets.Count > 0)
-		{
-			foreach (GameObject target in specialController.FOV.VisibleTargets)
-			{
-				Effects.Damage(target, (Damage + (damageMultiplier * Charges)) * (1 + GetComponentInParent<StatusEffectHandler>().DmgBoost), holderAgent);
-				Effects.ApplyForce(target, (target.transform.position - specialController.FOV.transform.position).normalized * (HitForce + (hitForceMultiplier * Charges)));
-				Effects.ApplyWeaponEffects(target, effects);
-			}
-		}
-		Charges = 0;
-	}
+    public override void StartTurnEffect()
+    {
+        base.AddCharge();
+    }
+    public override void RevivedEffect()
+    {
+        Charges = MaxCharges;
+    }
+
+    public override void DoSpecialAction()
+    {
+        if (specialController.FOV.VisibleTargets.Count > 0)
+        {
+            foreach (GameObject target in specialController.FOV.VisibleTargets)
+            {
+                Effects.Damage(target, (Damage + (damageMultiplier * Charges)) * (1 + GetComponentInParent<StatusEffectHandler>().DmgBoost), holderAgent);
+                Effects.ApplyForce(target, (target.transform.position - specialController.FOV.transform.position).normalized * (HitForce + (hitForceMultiplier * Charges)));
+                Effects.ApplyWeaponEffects(target, effects);
+            }
+        }
+        Charges = 0;
+    }
+    private IEnumerator CountdownTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        if (particleEffect.activeSelf)
+        {
+            particleEffect.SetActive(false);
+        }
+    }
 }
