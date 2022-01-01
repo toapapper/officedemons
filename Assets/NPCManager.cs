f8fcc6a7f5a2c1a844555041d49b1e96d6e17c66
tila18 <43038427+tila18@users.noreply.github.com>
2022-01-01 16:19:38 +0100

    continuation NPC

--- Assets/NPCManager.cs
+++ Assets/NPCManager.cs
@@ -5,30 +5,66 @@ using UnityEngine.UI;
 
 public class NPCManager : MonoBehaviour
 {
-    [SerializeField] GameObject npcTemplate;
+    [SerializeField] GameObject NPCs;
 
-    [SerializeField] List<Vector3> spawnPoints;
+    [SerializeField] List<GameObject> spawnPoints;
                          
-    [SerializeField] List<Vector3> exitPoints;
+    [SerializeField] List<GameObject> exitPoints;
 
     List<GameObject> npcList;
+    List<Vector3> spawnPointPositions;
+    List<Vector3> exitPointPositions;
 
-    int random;
+    [SerializeField]
+    float timeBetweenSpawns = 3f;
+
+    float timer = 0f;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         npcList = new List<GameObject>();
+        spawnPointPositions = new List<Vector3>();
+        exitPointPositions = new List<Vector3>();
+
+        foreach (GameObject go in spawnPoints)
+        {
+            spawnPointPositions.Add(go.transform.position);
+        }
+
+        foreach (GameObject go in exitPoints)
+        {
+            exitPointPositions.Add(go.transform.position);
+        }
+
+        foreach (Transform child in NPCs.transform)
+        {
+            npcList.Add(child.gameObject);
+        }
+        Debug.Log("NPC count:" + npcList.Count);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // update list
+        timer += Time.deltaTime;
+        float seconds = timer % 60;
+
+        if (seconds >= timeBetweenSpawns) // update list
+        {
+            AddNPC();
+            timer = 0;
+        }
+        
 
         foreach (GameObject npc in npcList)
         {
-            npc.GetComponent<NPCScript>().Update();
+            if (npc.active)
+            {
+                npc.GetComponent<NPCScript>().Update();
+            }
         }
     }
 
@@ -39,10 +75,19 @@ public class NPCManager : MonoBehaviour
 
     public void AddNPC()
     {
-        Vector3 spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count -1)];
-        Vector3 exitPoint = exitPoints[Random.Range(0, exitPoints.Count - 1)];
+        foreach (GameObject npc in npcList)
+        {
+            if (!npc.active)
+            {
+                Debug.Log("SPAWNED");
+                Vector3 spawnPoint = spawnPointPositions[Random.Range(0, spawnPoints.Count - 1)];
+                Vector3 exitPoint = exitPointPositions[Random.Range(0, exitPoints.Count - 1)];
 
-        GameObject newNPC = Instantiate(npcTemplate);
-        newNPC.GetComponent<NPCScript>().InstantiateValues(spawnPoint, exitPoint);
+                npc.GetComponent<NPCScript>().InstantiateValues(spawnPoint, exitPoint);
+
+                npc.SetActive(true);
+                break;
+            }
+        }
     }
 }
--- Assets/NPCScript.cs
+++ Assets/NPCScript.cs
@@ -5,48 +5,85 @@ using UnityEngine.AI;
 
 public class NPCScript : MonoBehaviour
 {
-    Vector3 targetPosition;
+    Vector3 exitPosition;
     Vector3 spawnPosition;
-    Vector3 position;
+    Vector3 currentTargetPosition;
     NavMeshAgent navMeshAgent;
     Attributes attributes;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-
-    }
+    // for random wandering
+    float wanderRadius = 10;
+    float wanderTimer = 3;
+    float timer = 0;
+    int wandersBeforeExit = 3; // How many times they take a random destination before going towards an exit
+    int counter = 0;
 
     public void Update()
     {
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            if (counter < wandersBeforeExit)
+            {
+                currentTargetPosition = RandomNavSphere(transform.position, wanderRadius, -1);
+                timer = 0;
+                counter += 1;
+            }
+            else
+            {
+                currentTargetPosition = exitPosition;
+            }
+        }
+
         // if hp <= 0 -> Die
-
-        // else move
+        if (attributes.Health <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            Debug.Log("spawn: " + spawnPosition + "  exit: " + exitPosition);
+            MoveTowards(currentTargetPosition);
+        }
+        
     }
 
-    // 
-    public void Move()
+    public void MoveTowards(Vector3 targetPos)
     {
-        // Claculate next step in navmesh 
-        // try to avoid players
-
+        navMeshAgent.isStopped = false;
+        navMeshAgent.SetDestination(targetPos);
     }
+    
 
     public void InstantiateValues(Vector3 start, Vector3 exit)
     {
-        targetPosition = exit;
+        exitPosition = exit;
         spawnPosition = start;
-        navMeshAgent = new NavMeshAgent();
-        position = spawnPosition;
+        currentTargetPosition = RandomNavSphere(transform.position, wanderRadius, -1);
+        
+        gameObject.transform.position = spawnPosition;
+        navMeshAgent = GetComponent<NavMeshAgent>();
         
         attributes = GetComponent<Attributes>();
-
     }
 
     public void Die()
     {
         // play dying effect
         // remove from list in manager
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
     }
 }
